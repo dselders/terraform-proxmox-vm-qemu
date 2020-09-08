bd7b1ffcfee85c22f4b863ea8395c1c779242825
David Selders <david.selders@gmail.com>
2020-09-07 22:02:55 -0700

    Correct documentation errors

--- examples/main.tf
+++ examples/main.tf
@@ -1,20 +1,5 @@
-provider "proxmox" {
-  pm_api_url      = "${var.proxmox_api_url}/api2/json"
-  pm_user         = var.proxmox_user
-  pm_password     = var.proxmox_password
-  pm_tls_insecure = var.proxmox_tls_insecure
-}
-
-provider "local" {
-  version = "~> 1.4"
-}
-
-data "local_file" "ssh_public_key" {
-  filename = "/home/david/.ssh/id_rsa.pub"
-}
-
 module "virtual_machines" {
-  source = "./modules/proxmox_vm"
+  source = "../modules/proxmox_vm"
 
   proxmox_vms = {
     test1 = {
